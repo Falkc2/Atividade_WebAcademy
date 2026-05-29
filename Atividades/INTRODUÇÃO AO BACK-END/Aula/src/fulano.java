5ed0f82ec67def831cad505b308712edfde7ce9e
Falkc2 <vsantosleite2@gmail.com>
2026-05-28 20:26:10 -0500

    ADD: Adicionando Diagrama

---
+++ Atividades/INTRODUÇÃO AO BACK-END/Aula/src/fulano.java
@@ -0,0 +1,14 @@
+public class fulano {
+    public static void main(String[] args) {
+        
+        int contador = 0;
+
+        while (contador < 99999999) {
+            System.out.println("Olá");
+            contador++;
+        }
+        
+
+    }
+
+}
